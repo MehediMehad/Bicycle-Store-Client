ab43b5669d6e67f54f0d0a8bcd8603d56179b474
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-01-27 21:28:23 +0600

    Navbar Refactor

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import logo from "../../assets/icons/logo.png";
 import { MdMenu } from "react-icons/md";
-import { CiSearch } from "react-icons/ci";
-import { PiShoppingCartThin } from "react-icons/pi";
 import ResponsiveMenu from "./ResponsiveMenu";
 // import ResponsiveMenu from "./ResponsiveMenu";
 const Navbar = () => {
@@ -10,9 +8,9 @@ const Navbar = () => {
   return (
     <>
       <nav className="bg-white shadow-md">
-        <div className=" container mx-auto flex justify-between items-center py-3">
+        <div className="relative container mx-auto flex items-center py-3 h-16 mt-1">
           {/* LOGO SECTION */}
-          <div className="text-2xl flex items-center gap-2 font-bold">
+          <div className="absolute left-0 text-2xl flex items-center justify-start gap-2 font-bold">
             <img className="w-12" src={logo} alt="" />
             <div className="flex mt-3">
               <p>Pedal-</p>
@@ -20,14 +18,14 @@ const Navbar = () => {
             </div>
           </div>
           {/* MENU SECTION */}
-          <div className="hidden md:block">
+          <div className="hidden md:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <ul className="flex items-center gap-6 text-back">
               {NavbarMenu.map((item) => {
                 return (
                   <li key={item.id}>
                     <a
                       href={item.link}
-                      className="inline-block py-1 px-3 hover:text-amber-500 font-semibold duration-200"
+                      className="inline-block py-1 px-3 hover:text-[#19a270] font-semibold duration-200"
                     >
                       {item.title}
                     </a>
@@ -37,19 +35,22 @@ const Navbar = () => {
             </ul>
           </div>
           {/* iCON SECTION */}
-          <div className="flex items-center gap-4">
-            <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
+          <div className="absolute right-0 flex items-center gap-4 ml-auto">
+            {/* <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
               <CiSearch />
             </button>
             <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
               <PiShoppingCartThin />
-            </button>
-            <button className=" font-semibold hover:cursor-pointer rounded-md border-2 px-6 py-2 duration-200 hidden md:block">
+            </button> */}
+            <button className="font-semibold hover:cursor-pointer hover:bg-[#19a270] rounded-md border-2 hover:border-2 text-back hover:text-white px-6 py-2 duration-200 hidden md:block">
               Login
             </button>
           </div>
           {/* MOBILE HAMBURGER MENU SECTION */}
-          <div className="md:hidden" onClick={() => setOpen(!open)}>
+          <div
+            className="md:hidden absolute right-0 mr-[2px] cursor-pointer"
+            onClick={() => setOpen(!open)}
+          >
             <MdMenu className="text-4xl" />
           </div>
         </div>
@@ -63,8 +64,14 @@ const Navbar = () => {
 export default Navbar;
 export const NavbarMenu = [
   { id: 1, title: "Home", link: "/" },
-  { id: 2, title: "Trainer", link: "#" },
-  { id: 3, title: "Program", link: "#" },
-  { id: 4, title: "Blogs", link: "#" },
-  { id: 5, title: "Pricing", link: "#" },
+  { id: 2, title: "Bicycles", link: "#" },
+  { id: 3, title: "About ", link: "#" },
+  { id: 4, title: "Dashboard", link: "#" },
+  // { id: 5, title: "Pricing", link: "#" },
 ];
+
+<div className="flex">
+  <div className="">1</div>
+  <div className="">2</div>
+  <div className="">3</div>
+</div>;
