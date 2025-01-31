114f97cabe65d118ec0521d5f627cbc2385afef9
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-01-31 20:35:28 +0600

    update: baseQuery

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:7000/api",
+  credentials: "include",
+});
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:7000/api",
-    credentials: "include",
-  }),
+  baseQuery,
   endpoints: () => ({}),
 });
