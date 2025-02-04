8d1759112d4cbf13cd5543bbbc2aa06d143ddeb7
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-02-04 11:52:13 +0600

    add: Create endpoints createOrder, getOrders, verifyOrder

--- src/redux/features/order/order.ts
+++ src/redux/features/order/order.ts
@@ -1,1 +1,49 @@
+import { TQueryParam, TResponseRedux } from "../../../types/global.type";
+import { baseApi } from "../../api/baseApi";
 
+const orderApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    createOrder: builder.mutation({
+      query: (userInfo) => ({
+        url: "/orders",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+    getOrders: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/orders",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<any[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    verifyOrder: builder.query({
+      query: (order_id) => ({
+        url: "/orders/verify",
+        params: { order_id },
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateOrderMutation,
+  useGetOrdersQuery,
+  useVerifyOrderQuery,
+} = orderApi;
